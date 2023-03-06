1f9fea8be63b9c10634814d665bdab5f72ab6f81
Eivind Hauge Halsnes <ehalsnes@hotmail.com>
2023-03-06 14:20:51 +0100

    Basic HTML skeleton for login-page. Added styles.css [200~width: 100px;   height: 100px;   position: absolute;   top: 50%;   left: 50%;

---
+++ pages/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styling/styles.css">
+    <title>Login-page</title>
+</head>
+<body>
+ 
+<div class=login-form>
+    <div class="email-form">
+        <h3>E-mail</h3>
+        <input type="text">
+    </div>
+
+    <div class="password-form">
+        <h3>Password</h3>
+        <input type="text">
+    </div>
+    
+    <div class="signup-form">
+        <p>Don't have an account? <a href="#">Sign up</a></p>
+    </div>
+
+    <div class="forgot-password-form">
+        <p>Forgot password? <a href="#">Click here</a></p>
+    </div>
+
+    <div class="remember-password-checkbox">
+        <input type="checkbox" id="remember-password" name="remember-password">
+        <label for="husk-passord">Remember password</label>
+    </div>
+</div>
+</body>
+</html>
