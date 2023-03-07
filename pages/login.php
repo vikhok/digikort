c76267b96ffd0f104db6870c9ededea5f3dd81a5
ehalsnes <ehalsnes@hotmail.com>
2023-03-07 14:48:09 +0100

    Changed div structure and more styling

--- pages/login.php
+++ pages/login.php
@@ -9,29 +9,40 @@
 </head>
 <body>
  
-<div class=login-form>
-    <div class="email-form">
-        <h3>E-mail</h3>
-        <input type="text">
-    </div>
+<div class="login-form">
+    <div class="login-container">    
+        <form action="">
 
-    <div class="password-form">
-        <h3>Password</h3>
-        <input type="text">
-    </div>
-    
-    <div class="signup-form">
-        <p>Don't have an account? <a href="#">Sign up</a></p>
-    </div>
+            <div class="digikort-heading">
+                <h1>DigiKort</h1>
+            </div>
+            
+            <div class="email-form">
+                <h3>E-mail</h3>
+                <input type="text" class="form-control" required>
+            </div>
 
-    <div class="forgot-password-form">
-        <p>Forgot password? <a href="#">Click here</a></p>
-    </div>
+            <div class="password-form">
+                <h3>Password</h3>
+                <input type="password" class="form-control" required>
+            </div>
 
-    <div class="remember-password-checkbox">
-        <input type="checkbox" id="remember-password" name="remember-password">
-        <label for="husk-passord">Remember password</label>
-    </div>
-</div>
+            <div class="remember-password-checkbox">
+                <input type="checkbox" id="remember-password" name="remember-password">
+                <label for="husk-passord">Remember password</label>
+            </div>
+        
+            <div class="signup-form">
+                <p>Don't have an account? <a href="#">Sign up</a></p>
+            </div>
+
+            <div class="forgot-password-form">
+                <p>Forgot password? <a href="#">Click here</a></p>
+            </div>
+
+            <input type="submit" class="btn" value="Login">
+        </form>
+        </div>
+        </div>
 </body>
 </html>
