0605ea55d6a1f69cc1e075aae52198de82ff87df
Haakon Kristiansen <haakon128@gmail.com>
2023-03-10 11:34:00 +0100

    Oppdaterte html og css fil

--- pages/rediger_profil.php
+++ pages/rediger_profil.php
@@ -16,27 +16,37 @@
     <form action="rediger_profil.php" method="POST">
 
         <label for="full_name">Fullt navn:</label>
-        <input type="text" id="full_name" name="full_name"><br><br>
+        <input type="text" id="full_name" name="full_name" placeholder="Navnet ditt"><br><br>
 
         <label for="stillingstittel">Stillingstittel:</label>
-        <input type="text" id="stillingstittel" name="stillingstittel"><br><br>
+        <input type="text" id="stillingstittel" name="stillingstittel" placeholder="Utvikler"><br><br>
 
         <label for="email">E-post:</label>
-        <input type="email" id="email" name="email"><br><br>
+        <input type="email" id="email" name="email" placeholder="eksempel@epost.no" ><br><br>
 
         <label for="telefon">Telefon:</label>
-        <input type="text" id="telefon" name="telefon"><br><br>
+        <input type="text" id="telefon" name="telefon" placeholder="+47 12345678" ><br><br>
 
+    <div class="rediger_some">
+
+        <div class="column">
         <label for="linkedin">LinkedIn:</label>
         <input type="url" id="linkedin" name="linkedin" placeholder="https://linkedin.com/"><br><br>
+        </div>
 
+        <div class="column">
         <label for="github">Github:</label>
         <input type="url" id="github" name="github" placeholder="https://github.com/"><br><br>
+        </div>
 
+        <div class="column">
         <label for="instagram">Instagram:</label>
         <input type="url" id="instagram" name="instagram" placeholder="https://www.instagram.com/"><br><br>
+        </div>
 
         <button type="submit">Oppdater profil</button>
+
+    </div>
     </form>
 </div>
 
