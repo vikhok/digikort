a07ebb2bb4288f6ac86c08d99aab1bc35e56e699
AleksKol <kols203@gmail.com>
2023-03-13 10:36:20 +0100

    Rework-login changes

--- pages/login.php
+++ pages/login.php
@@ -5,44 +5,46 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styling/styles.css">
-    <title>Login-page</title>
+    <link rel="stylesheet" href="fonts/fontawesome-free-6.3.0-web/fontawesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <title>Login</title>
 </head>
 <body>
  
 <div class="login-form">
     <div class="login-container">    
-        <form action="">
+        <form name="login" class="login" action="POST">
 
             <div class="digikort-heading">
                 <h1>DigiKort</h1>
             </div>
             
             <div class="email-form">
-                <h3>E-mail</h3>
+                <label>E-postadresse</label>
                 <input type="text" class="form-control" required>
             </div>
 
             <div class="password-form">
-                <h3>Password</h3>
+                <label>Passord</label>
                 <input type="password" class="form-control" required>
             </div>
 
             <div class="remember-password-checkbox">
                 <input type="checkbox" id="remember-password" name="remember-password">
-                <label for="husk-passord">Remember password</label>
+                <label for="husk-passord">Husk passord</label>
             </div>
         
             <div class="signup-form">
-                <p>Don't have an account? <a href="#">Sign up</a></p>
-            </div>
+                <p>Mangler du brukerkonto? <a href="#">Registerer konto</a></p>
+            </div> 
 
             <div class="forgot-password-form">
-                <p>Forgot password? <a href="#">Click here</a></p>
+                <p>Glemt passord? <a href="#">Trykk her</a></p>
             </div>
 
-            <input type="submit" class="btn" value="Login">
+            <input type="submit" class="submit" value="Login">
         </form>
-        </div>
-        </div>
+    </div>
+</div>
 </body>
 </html>
