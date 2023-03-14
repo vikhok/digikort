101dcf6c7557dea60c5e0a700e9003a436da2269
vikhok <vikhokjoh@gmail.com>
2023-03-14 10:54:47 +0100

    Header inc fixed

--- assets/include/header.inc.php
+++ assets/include/header.inc.php
@@ -1,15 +1,21 @@
 <?php function banner($profile_picture = false) { ?>
-    <?php if($profile_picture) { ?>
-        <header>
+    <header>
+        <?php if($profile_picture) { ?>
             <img class="profile_picture" src="/digikort/profiles/profile1/profile_pic.png" alt="Profile picture">
+        <?php } ?>
             <nav>
                 <ul class="nav__links">
-                    <li><a href="#">Visittkort</a></li>
-                    <li><a href="#">Rediger profil</a></li>
-                    <li><a href="#">Bedrift</a></li>
-                    <li><a href="#">Log ut</a></li>
+                    <?php if(isset($SESSION)/* && $ISLOGGEDIN == true */) { ?>
+                        <li><a href="#">Visittkort</a></li>
+                        <li><a href="#">Rediger profil</a></li>
+                        <li><a href="#">Bedrift</a></li>
+                        <li><a href="#">Logg ut</a></li>
+                    <?php } else { ?>
+                        <li><a href="#">Visittkort</a></li>
+                        <li><a href="#">Bedrift</a></li>
+                        <li><a href="#">Logg inn</a></li>
+                    <?php } ?>
                 </ul>
             </nav>
         </header>
-    <?php } ?>
 <?php } ?>
