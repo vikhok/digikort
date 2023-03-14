0545dccb86232182eb9ebbeb2fb6110bd85bcf8b
Haakon Kristiansen <haakon128@gmail.com>
2023-03-14 11:49:27 +0100

    Flere endringer i styling

--- pages/rediger_profil.php
+++ pages/rediger_profil.php
@@ -23,32 +23,32 @@
 
     <form action="rediger_profil.php" method="POST">
 
-        <label for="full_name">Fullt navn:</label>
+        <label for="full_name">Fullt navn</label>
         <input type="text" id="full_name" name="full_name" placeholder="Navnet ditt"><br><br>
 
-        <label for="stillingstittel">Stillingstittel:</label>
-        <input type="text" id="stillingstittel" name="stillingstittel" placeholder="Utvikler"><br><br>
+        <label for="stillingstittel">Stillingstittel</label>
+        <input type="text" id="stillingstittel" name="stillingstittel" placeholder="Din stillingstittel"><br><br>
 
-        <label for="email">E-post:</label>
+        <label for="email">E-post</label>
         <input type="email" id="email" name="email" placeholder="eksempel@epost.no" ><br><br>
 
-        <label for="telefon">Telefon:</label>
+        <label for="telefon">Telefon</label>
         <input type="text" id="telefon" name="telefon" placeholder="+47 12345678" ><br><br>
 
         <div class="rediger_some">
 
             <div class="column">
-            <label for="linkedin">LinkedIn:</label>
+            <label for="linkedin">LinkedIn</label>
             <input type="url" id="linkedin" name="linkedin" placeholder="https://linkedin.com/"><br><br>
             </div>
 
             <div class="column">
-            <label for="github">Github:</label>
+            <label for="github">Github</label>
             <input type="url" id="github" name="github" placeholder="https://github.com/"><br><br>
             </div>
 
             <div class="column">
-            <label for="instagram">Instagram:</label>
+            <label for="instagram">Instagram</label>
             <input type="url" id="instagram" name="instagram" placeholder="https://www.instagram.com/"><br><br>
             </div>
         
