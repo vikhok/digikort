c16c6b06b9504bdcac2f6c13d63e15df721f36c0
vikhok <vikhokjoh@gmail.com>
2023-03-14 11:50:55 +0100

    Hamburger-menu functionality with JS

--- assets/include/header.inc.php
+++ assets/include/header.inc.php
@@ -3,19 +3,25 @@
         <?php if($profile_picture) { ?>
             <img class="profile_picture" src="/digikort/profiles/profile1/profile_pic.png" alt="Profile picture">
         <?php } ?>
-            <nav>
-                <ul class="nav__links">
+            <nav class="navbar">
+                <ul class="nav-menu">
                     <?php if(isset($SESSION)/* && $ISLOGGEDIN == true */) { ?>
-                        <li><a href="#">Visittkort</a></li>
-                        <li><a href="#">Rediger profil</a></li>
-                        <li><a href="#">Bedrift</a></li>
-                        <li><a href="#">Logg ut</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Visittkort</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Rediger profil</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Bedrift</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Logg ut</a></li>
                     <?php } else { ?>
-                        <li><a href="#">Visittkort</a></li>
-                        <li><a href="#">Bedrift</a></li>
-                        <li><a href="#">Logg inn</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Visittkort</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Bedrift</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Logg inn</a></li>
                     <?php } ?>
                 </ul>
+                <div class="hamburger">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </div>
             </nav>
         </header>
+        <script src="/digikort/assets/include/menu.js"></script>
 <?php } ?>
