da7eb6fe5524db9ac1ddc75c5f328ab42d20521d
AleksKol <kols203@gmail.com>
2023-03-20 11:59:15 +0100

    Merge styling-dokuments and html-cleanup

--- pages/Registrer-konto.php
+++ pages/Registrer-konto.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styling/Styles-registrer-konto.css">
+    <link rel="stylesheet" href="styling/Styles-login.css">
     <link rel="stylesheet" href="fonts/fontawesome-free-6.3.0-web/fontawesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
     <title>Registrer</title>
@@ -18,29 +18,13 @@
         <div class="digikort-heading">
             <h1>DigiKort</h1>
         </div>
-
-        <div class="firstname-form">
-            <input type="text" class="form-control" placeholder="Fornavn" required>
-        </div>
-
-        <div class="surname-form">
-            <input type="text" class="form-control" placeholder="Etternavn" required>
-        </div>
-
-        <div class="email-register-form">
-            <input type="text" class="form-control" placeholder="Email" required>
-        </div>
-
-        <div class="phonenumber-form">
-            <input type="tel" class="form-control" placeholder="Mobilnummer" required>
-        </div>
-
-        <div class="register-password-form">
-            <input type="password" class="form-control" placeholder="Passord" required>
-        </div>
-        
-        <div class="confirm-password-form">
-            <input type="password" class="form-control" placeholder="Gjenta Passord" required>
+        <div class="form-control">
+            <input class="firstname-form" type="text"  placeholder="Fornavn" required>
+            <input class="surname-form" type="text"  placeholder="Etternavn" required>
+            <input class="email-register-form" type="text"  placeholder="Email" required>
+            <input class="phonenumber-form" type="tel"  placeholder="Mobilnummer" required>
+            <input class="register-password-form" type="password"  placeholder="Passord" required>
+            <input class="confirm-password-form" type="password"  placeholder="Gjenta Passord" required>
         </div>
 
         <div>
