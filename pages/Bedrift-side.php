7b2d3262a97df9dc2dc7e32b50ecec06044ec723
Aleksander <kols203@gmail.com>
2023-03-22 14:44:19 +0100

    Knappene i Bedrift-siden er nå stylet riktig

--- pages/Bedrift-side.php
+++ pages/Bedrift-side.php
@@ -13,25 +13,26 @@
 
     <body>
         <?php banner(false) ?>
+        <div class="Bedrift-siden">
+            <div class="Bedrift-bilde">
 
-        <div class="Bedrift-bilde">
-
-        </div>
-
-        <div class="Bedrift-Text">
-            <h2 class="bedrift-tittel">Egde AS</h2>
-            <h3 class="bedrift-slogan">Vi forener mennesker og teknologi.</h3>
-            <p class="bedrift-setning">Sammen utvider vi horisonten for hvordan teknologi kan bidra til at virksomheter lykkes.</p>
-        </div>
-
-        <div class="Bedrift-knapper">
-            <div class="om-og-kontakt-knapp">
-            <a class="a_om_oss" href="https://egde.no/om-oss/">Om Oss</a>
-            <a class="a-kontakt-oss" href="https://egde.no/kontakt/">Se ansatte</a>
             </div>
-            <div class="veibeskrivelse-knapp">
-            <a class="a_veibeskrivelse" href="https://www.google.com/maps/place/Egde+Consulting+AS/@58.1401491,7.9944956,15.93z/data=!4m6!3m5!1s0x463802612c6a6bc5:0xec7da6512d9d2ce0!8m2!3d58.1409102!4d7.9948404!16s%2Fg%2F11c7t422qw">Veibeskrivelse på kart</a>
+
+            <div class="Bedrift-Text">
+                <h2 class="bedrift-tittel">Egde AS</h2>
+                <h3 class="bedrift-slogan">Vi forener mennesker og teknologi.</h3>
+                <p class="bedrift-setning">Sammen utvider vi horisonten for hvordan teknologi kan bidra til at virksomheter lykkes.</p>
             </div>
+
+            <div class="Bedrift-knapper">
+                <div class="om-og-kontakt-knapp">
+                <a class="a_om_oss" href="https://egde.no/om-oss/">Om Oss</a>
+                <a class="a-kontakt-oss" href="https://egde.no/kontakt/">Se ansatte</a>
+                </div>
+                <div class="veibeskrivelse-knapp">
+                <a class="a_veibeskrivelse" href="https://www.google.com/maps/place/Egde+Consulting+AS/@58.1401491,7.9944956,15.93z/data=!4m6!3m5!1s0x463802612c6a6bc5:0xec7da6512d9d2ce0!8m2!3d58.1409102!4d7.9948404!16s%2Fg%2F11c7t422qw">Veibeskrivelse på kart</a>
+                </div>
+             </div>
          </div>
 
         
