64390abcbbaa5ea620faddf0c8fdf485f7e5375f
ehalsnes <ehalsnes@hotmail.com>
2023-04-13 13:47:08 +0200

    Added more styling of contact-form and some restruction. Needs implementation of PHPMailer

--- pages/contact-employee.php
+++ pages/contact-employee.php
@@ -6,6 +6,23 @@
     session_start();
     $_SESSION["site"]["last_visited"] = $_SERVER["REQUEST_URI"];
 
+    $user_id = $_REQUEST["user_id"];
+    if($user = get_user($user_id)) {
+        $name = $user->first_name . " " . $user->last_name;
+        $email = $user->email;
+        $phone = $user->phone;
+        $job_title = null;
+        $company = null;
+    } else {
+        $failed = "<h4><span style='color:red'>
+        Noe gikk galt, fant ikke bruker i systemet.
+        </span></h4>";
+    }
+    if($user_company = get_user_company($user_id)) {
+        $job_title = $user_company->job_title;
+        $company = $user_company->company_name;
+    }
+
 ?>
 
 <!DOCTYPE html>
@@ -20,21 +37,22 @@
     <title>Kontaktskjema</title>
 </head>
 <body>
+    <?php banner($user_id, false) ?>
     <div class="contact-form" id="fields">
         <form name="contact" method="POST"> 
             <section class="name-section">
                 <label for="name">Navn</label>
-                <input type="text" id="name" name="name" size="100">
+                <input type="text" id="name" name="name" size="100" required>
             </section>
 
             <section class="email-section">
                 <label for="email">E-post</label>
-                <input type="text" id="email" name="email" size="100">
+                <input type="text" id="email" name="email" required>
             </section>
 
-            <section class="freetext-section">
+            <section class="freetext-section" id="freetext">
                 <label for="message">Din melding</label>
-                <input type="text" id="message" name="message" size="100" require>
+                <textarea name="freetext" cols="25" rows="10" name="description" wrap="physical" required></textarea>
             </section>
 
             <section class="submit-button">
@@ -42,5 +60,6 @@
             </section>
         </form>
     </div>
+    <?php footer("profile"); ?>
 </body>
 </html>
