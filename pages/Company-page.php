111f2ed0d5f48e950a8bf807c54ef4b4afdf3c03
AleksKol <kols203@gmail.com>
2023-04-14 10:39:06 +0200

    Created company-page, fikset bildet

--- pages/Company-page.php
+++ pages/Company-page.php
@@ -1,8 +1,20 @@
 <?php
     require_once("../assets/include/header.inc.php");
-    require_once("../assets/include/footer.inc.php");
+    require_once("../assets/include/db.inc.php");
 
     session_start();
+    $_SESSION["site"]["last_visited"] = $_SERVER["REQUEST_URI"];
+
+    $company_id = $_REQUEST["company_id"];
+    if($location = get_location($company_id)) {
+        $address = $location->address;
+        $city = $location->city;
+        $zip = $location->zip;
+    } else {
+        $failed = "<h4><span style='color:red'>
+        Noe gikk galt, fant ikke bedriften i systemet.
+        </span></h4>";
+    }
 
     $company_id = $_REQUEST["company_id"];
 ?>
@@ -15,10 +27,10 @@
 <link rel="stylesheet" href="../assets/styles/styles.css">
 <title>Bedrift</title>
 <body>
-    <?php banner(false, $company_id) ?>
+    <?php banner(false) ?>
     <div class="Bedrift-siden">
         <div class="Bedrift-bilde">
-            <img src="../Companies/Company1/Egde_Grimstad.png" alt="Bilde av bedriften">
+            <img class="Bedrift-bilde-styling" src="../Companies/Company1/Egde_Grimstad.png" alt="Bilde av bedriften">
         </div>
 
         <div class="Bedrift-Text">
@@ -37,6 +49,31 @@
             </div>
             </div>
         </div>
+        <?php
+        // Build API address
+        $full_address = "$address $zip $city";
+        $url = "https://ws.geonorge.no/adresser/v1/sok?fuzzy=true&sok=";
+        $url .= str_replace(" ", "%20", $full_address);
+        $url .= "&treffPerSide=1&asciiKompatibel=true";
+
+        // Get result from Geonorges API
+        $ch = curl_init();
+        $param = [CURLOPT_URL => $url, CURLOPT_RETURNTRANSFER => true];
+        curl_setopt_array($ch, $param);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        $searchResult = json_decode($result, true);
+
+        // Utilzie longitude and latitude from API result
+        if(isset($searchResult["adresser"][0])) {
+            $lat = $searchResult["adresser"][0]["representasjonspunkt"]["lat"];
+            $lon = $searchResult["adresser"][0]["representasjonspunkt"]["lon"];
+            // Display Google Maps on web-page using an iframe
+            echo '<iframe class="map" src="https://maps.google.com/maps?q=' . $lat . ', ' . $lon . '&z=16&output=embed" width="50%" height="500px" frameborder="0" style="border:0"></iframe>';
+        } else {
+            echo "Kunne ikke generere kart for adressen.";
+        }
+    ?>
 </body>
 </head>
 </html>
