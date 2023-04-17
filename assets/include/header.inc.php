5e0f22e0c3f314a589a3b55e35f75a528d2d1d3c
vikhok <vikhokjoh@gmail.com>
2023-04-17 09:56:55 +0200

    Fixed problem in header.inc and commented code

--- assets/include/header.inc.php
+++ assets/include/header.inc.php
@@ -7,41 +7,45 @@
 -->
 <?php function banner($user_id = false, $company_id = false) { ?>
     <header>
-        <?php if($user_id) { 
+        <?php if($user_id) {
+            // Define and display profile picture:
             $folder = md5("user." . $user_id);
             $dir = "../profiles/" . $folder . "/profile_picture.png";
             if(!file_exists($dir)) {
                 $dir = "../profiles/stockprofile/profile_picture.png";
             }
+            echo "<img class='profile_picture' src='$dir' alt='Profile picture'>";
+        }
         ?>
-        <img class="profile_picture" src="<?=$dir?>" alt="Profile picture">
-        <?php } ?>
-            <nav class="navbar">
-                <ul class="nav-menu">
-                    <?php if(isset($_SESSION["user"]["logged_in"]) && $_SESSION["user"]["logged_in"] == true) { ?>
-                        <li class="nav-item"><a href="index.php?user_id=<?= $_SESSION["user"]["user_id"] ?>" class="nav-link">Mitt visittkort</a></li>
-                        <li class="nav-item"><a href="mine-notater.php?user_id=<?= $_SESSION["user"]["user_id"] ?>" class="nav-link">Mine notater</a></li>
-                        <li class="nav-item"><a href="rediger_profil.php?user_id=<?= $_SESSION["user"]["user_id"] ?>" class="nav-link">Rediger profil</a></li>
-                        <?php if($company_id) { ?>
-                            <li class="nav-item"><a href="company-page.php?company_id=<?=$company_id?>" class="nav-link">Bedrift</a></li>
-                        <?php } else { ?>
-                            <li class="nav-item"><a href="company-join.php" class="nav-link">Bli med i bedrift</a></li>
-                        <?php } ?>
-                        <li class="nav-item"><a href="utility/logout.php" class="nav-link">Logg ut</a></li>
+        <nav class="navbar">
+            <ul class="nav-menu">
+                <?php // Check whether user is logged in:
+                    if(isset($_SESSION["user"]["logged_in"]) && $_SESSION['user']['logged_in'] == true) { ?>
+                    <li class="nav-item"><a href="index.php?user_id=<?=$_SESSION['user']['user_id']?>" class="nav-link">Mitt visittkort</a></li>
+                    <li class="nav-item"><a href="mine-notater.php?user_id=<?=$_SESSION['user']['user_id']?>" class="nav-link">Mine notater</a></li>
+                    <li class="nav-item"><a href="rediger_profil.php?user_id=<?=$_SESSION['user']['user_id']?>" class="nav-link">Rediger profil</a></li>
+                    <?php // Check whether user is belong in a company:
+                    if($company_id) { ?>
+                        <li class="nav-item"><a href="company-page.php?company_id=<?=$company_id?>" class="nav-link">Bedrift</a></li>
                     <?php } else { ?>
-                        <li class="nav-item"><a href="index.php?user_id=<?= $_SESSION["user"]["last_visited"] ?>" class="nav-link">Visittkort</a></li>
-                        <?php if($company_id) { ?>
-                            <li class="nav-item"><a href="company-page.php?company_id=<?=$company_id?>" class="nav-link">Bedrift</a></li>
-                        <?php } ?>
-                        <li class="nav-item"><a href="login.php" class="nav-link">Logg inn</a></li>
+                        <li class="nav-item"><a href="company-join.php" class="nav-link">Bli med i bedrift</a></li>
                     <?php } ?>
-                </ul>
-                <div class="hamburger">
-                    <span class="bar"></span>
-                    <span class="bar"></span>
-                    <span class="bar"></span>
-                </div>
-            </nav>
-        </header>
-        <script src="../assets/include/js/menu.js"></script>
+                    <li class="nav-item"><a href="utility/logout.php" class="nav-link">Logg ut</a></li>
+                <?php } else { ?>
+                    <li class="nav-item"><a href="index.php?user_id=<?=$_SESSION['user']['last_visited']?>" class="nav-link">Visittkort</a></li>
+                    <?php // Check whether user is belong in a company:
+                    if($company_id) { ?>
+                        <li class="nav-item"><a href="company-page.php?company_id=<?=$company_id?>" class="nav-link">Bedrift</a></li>
+                    <?php } ?>
+                    <li class="nav-item"><a href="login.php" class="nav-link">Logg inn</a></li>
+                <?php } ?>
+            </ul>
+            <div class="hamburger">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+        </nav>
+    </header>
+    <script src="../assets/include/js/menu.js"></script>
 <?php } ?>
