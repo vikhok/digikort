1af9b7f12b56f45ce98b003807622ba2ce0fc262
AleksKol <kols203@gmail.com>
2023-04-19 11:57:54 +0200

    Lagde Change-password page, ikke ferdig

---
+++ pages/change-password.php
@@ -0,0 +1,26 @@
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styling/login.css">
+    <link rel="stylesheet" href="fonts/fontawesome-free-6.3.0-web/fontawesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <title>Reset passord</title>
+</head>
+<body class="login-page">
+    <div class="bytt-passord-form">
+        <div class="bytt-passord-container">
+            <form name="Bytt Passord" method="POST" action="#">
+                <h1 class="digikort-heading">DigiKort</h1>
+            </form>
+
+            <div class="form-control">
+                <input type="password" name="Nytt passord" class="new-password-form" placeholder="Nytt passord" required>
+            </div>
+
+        </div>
+    </div>
+</body>
