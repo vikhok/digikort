5d71a10daa0c5d281d21bdea46bf94e41f119df7
vikhok <vikhokjoh@gmail.com>
2023-05-23 23:32:23 +0200

    docker oppsett med ssl

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,37 @@
 #PHP image
+# FROM php:8.0-apache
+# COPY . /var/www/html
+# WORKDIR /var/www/html
+# RUN docker-php-ext-install pdo pdo_mysql gd
+# EXPOSE 80
+
+#PHP image
+# FROM php:8.0-apache
+# RUN apt-get update && apt-get install -y \
+#     libfreetype6-dev \
+#     libjpeg62-turbo-dev \
+#     libpng-dev
+# RUN docker-php-ext-configure gd --with-freetype --with-jpeg
+# RUN docker-php-ext-install pdo pdo_mysql gd
+# COPY . /var/www/html
+# WORKDIR /var/www/html
+# EXPOSE 80
+
+
 FROM php:8.0-apache
+RUN apt-get update && apt-get install -y \
+    libfreetype6-dev \
+    libjpeg62-turbo-dev \
+    libpng-dev
+RUN docker-php-ext-configure gd --with-freetype --with-jpeg
+RUN docker-php-ext-install pdo pdo_mysql gd
+RUN a2enmod ssl
 COPY . /var/www/html
 WORKDIR /var/www/html
-RUN docker-php-ext-install mysqli && docker-php-ext-enable mysqli
+COPY ssl/apache_ssl.conf /etc/apache2/conf-available/ssl.conf
+COPY ssl/certificate.crt /etc/apache2/certificate.crt
+COPY ssl/private.key /etc/apache2/private.key
+RUN a2enconf ssl
+EXPOSE 80
+EXPOSE 443
+CMD ["apache2-foreground"]
--- assets/include/db.inc.php
+++ assets/include/db.inc.php
@@ -1,15 +1,51 @@
 <?php
-    define("DB_HOST", getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost');
-    define("DB_USER", getenv('DB_USER') ? getenv('DB_USER') : 'root');
-    define("DB_PASS", getenv('DB_PASS') ? getenv('DB_PASS') : '');
-    define("DB_NAME", getenv('DB_NAME') ? getenv('DB_NAME') : 'digikort');
+    // ini_set('display_errors', 1);
+    // ini_set('display_startup_errors', 1);
+    // error_reporting(E_ALL);    
+
+    // define("DB_HOST", getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost:3306');
+    // define("DB_USER", getenv('DB_USER') ? getenv('DB_USER') : 'root');
+    // define("DB_PASS", getenv('DB_PASS') ? getenv('DB_PASS') : 'pass');
+    // define("DB_NAME", getenv('DB_NAME') ? getenv('DB_NAME') : 'digikort');
+
+    
+    
+    define("DB_HOST", "mysql-digikort.mysql.database.azure.com");
+    define("DB_USER", "sadigikort");
+    define("DB_PASS", "J53KuXrfZbvGxU");
+    define("DB_NAME", "digikort");
+
+    $options = [
+        PDO::MYSQL_ATTR_SSL_CA => '/etc/apache2/certificate.crt',
+        PDO::MYSQL_ATTR_SSL_CERT => '/etc/apache2/certificate.crt',
+        PDO::MYSQL_ATTR_SSL_KEY => '/etc/apache2/private.key',
+    ];
+
     $dsn = "mysql:dbname=" . DB_NAME . ";host=" . DB_HOST;
     $pdo;
     try {
-        $pdo = new PDO($dsn, DB_USER, DB_PASS);
+        $pdo = new PDO($dsn, DB_USER, DB_PASS, $options);
     } catch (PDOException $e) {
-        //echo "Error connecting to database: " . $e->getMessage();
+        echo "Error connecting to database: " . $e->getMessage();
     }
+
+
+    // $host = 'localhost';
+    // $dbname = 'digikort';
+    // $username = 'root';
+    // $password = 'pass';
+    // $socket = '/var/run/mysqld/mysqld.sock';
+    //;unix_socket=$socket
+
+   
+
+    // try {
+    //     $pdo = new PDO("mysql:dbname=$dbname", $username, $password);
+    //     echo "Connected successfully";
+    // } catch (PDOException $e) {
+    //     die("Connection failed: " . $e->getMessage());
+    // }
+
 
     function login($email, $password) {
         global $pdo;
@@ -24,7 +60,7 @@
         try {
             $query->execute();
         } catch (PDOException $e) {
-            //echo $e->getMessage();
+            echo $e->getMessage();
             return false;
         }
 
