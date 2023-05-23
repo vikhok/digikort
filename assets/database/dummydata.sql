22d6e1227e36015614a6d3111c188846e7b984c9
AleksKol <kols203@gmail.com>
2023-05-23 09:45:37 +0200

    Lagt til Hallgeirs bakeri og Janis Jarnitsjarer

--- assets/database/dummydata.sql
+++ assets/database/dummydata.sql
@@ -2,24 +2,24 @@
 INSERT INTO user(first_name, last_name, job_title, email, phone, pass) VALUES 
     ('Eivind', 'Halsnes', 'Senior systemarkitekt', 'ehalsnes@epost.com', '12345678', '$2y$10$DW5DBzBM7hes7GrzItn2huzQKXM3yX52T5ZvyPr.PyDT537AufolK'),
     ('Viktor', 'Hokland', 'Administrerende direktør', 'vikhokjoh@gmail.com', '12345678', '$2y$10$DW5DBzBM7hes7GrzItn2huzQKXM3yX52T5ZvyPr.PyDT537AufolK'),
-    ('Aleksander', 'Kolsrud', 'Front-end utvikler', 'alekkols@epost.com', '12345678', '$2y$10$DW5DBzBM7hes7GrzItn2huzQKXM3yX52T5ZvyPr.PyDT537AufolK');
-    ('Arvid', 'Hellstrøm', 'Arkitekt', 'ArvHel@epost.com', '12345678', '$2y$10$DW5DBzBM7hes7GrzItn2huzQKXM3yX52T5ZvyPr.PyDT537AufolK');
-    ('Henrik', 'Hjaldar', 'Praktikant', 'Henhja@epost.com', '12345678', '$2y$10$DW5DBzBM7hes7GrzItn2huzQKXM3yX52T5ZvyPr.PyDT537AufolK');
-    ('Julius', 'Apekatt', 'Sirkus ape', 'Julape@epost.com', '12345678', '$2y$10$DW5DBzBM7hes7GrzItn2huzQKXM3yX52T5ZvyPr.PyDT537AufolK');
-    ('Mikael', 'Mannemann', 'Senior Front-end utvikler','Mikman@epost.com', '12345678', '$2y$10$DW5DBzBM7hes7GrzItn2huzQKXM3yX52T5ZvyPr.PyDT537AufolK');
-    ('Kristian', 'Sund', 'Direktør', 'Krisun@epost.com', '12345678', '$2y$10$DW5DBzBM7hes7GrzItn2huzQKXM3yX52T5ZvyPr.PyDT537AufolK');
-    ('Ole', 'Kristiansen', 'Praktikant', 'Olekri@epost.com', '12345678', '$2y$10$DW5DBzBM7hes7GrzItn2huzQKXM3yX52T5ZvyPr.PyDT537AufolK');
-    ('Dole', 'Didriksen', 'Junior Utvikler', 'Doldid@epost.com', '12345678', '$2y$10$DW5DBzBM7hes7GrzItn2huzQKXM3yX52T5ZvyPr.PyDT537AufolK');
-    ('Doffen', 'Kragerud', 'Database Ekspert', 'Dofkra@epost.com', '12345678', '$2y$10$DW5DBzBM7hes7GrzItn2huzQKXM3yX52T5ZvyPr.PyDT537AufolK');
+    ('Aleksander', 'Kolsrud', 'Front-end utvikler', 'alekkols@epost.com', '12345678', '$2y$10$DW5DBzBM7hes7GrzItn2huzQKXM3yX52T5ZvyPr.PyDT537AufolK'),
+    ('Arvid', 'Hellstrøm', 'Arkitekt', 'ArvHel@epost.com', '12345678', '$2y$10$DW5DBzBM7hes7GrzItn2huzQKXM3yX52T5ZvyPr.PyDT537AufolK'),
+    ('Henrik', 'Hjaldar', 'Praktikant', 'Henhja@epost.com', '12345678', '$2y$10$DW5DBzBM7hes7GrzItn2huzQKXM3yX52T5ZvyPr.PyDT537AufolK'),
+    ('Julius', 'Apekatt', 'Sirkus ape', 'Julape@epost.com', '12345678', '$2y$10$DW5DBzBM7hes7GrzItn2huzQKXM3yX52T5ZvyPr.PyDT537AufolK'),
+    ('Mikael', 'Mannemann', 'Senior Front-end utvikler','Mikman@epost.com', '12345678', '$2y$10$DW5DBzBM7hes7GrzItn2huzQKXM3yX52T5ZvyPr.PyDT537AufolK'),
+    ('Kristian', 'Sund', 'Direktør', 'Krisun@epost.com', '12345678', '$2y$10$DW5DBzBM7hes7GrzItn2huzQKXM3yX52T5ZvyPr.PyDT537AufolK'),
+    ('Ole', 'Kristiansen', 'Praktikant', 'Olekri@epost.com', '12345678', '$2y$10$DW5DBzBM7hes7GrzItn2huzQKXM3yX52T5ZvyPr.PyDT537AufolK'),
+    ('Dole', 'Didriksen', 'Junior Utvikler', 'Doldid@epost.com', '12345678', '$2y$10$DW5DBzBM7hes7GrzItn2huzQKXM3yX52T5ZvyPr.PyDT537AufolK'),
+    ('Doffen', 'Kragerud', 'Database Ekspert', 'Dofkra@epost.com', '12345678', '$2y$10$DW5DBzBM7hes7GrzItn2huzQKXM3yX52T5ZvyPr.PyDT537AufolK'),
     ('Donald', 'Durian', 'Daglig leder', 'Dondur@epost.com', '12345678', '$2y$10$DW5DBzBM7hes7GrzItn2huzQKXM3yX52T5ZvyPr.PyDT537AufolK');
 
 #Testdata user_social
 INSERT INTO user_social(user_id, linkedin, github, instagram) VALUES 
     ('1', 'https://www.linkedin.com/in/eivind-hauge-halsnes-875542215/', 'https://github.com/ehalsnes', 'https://www.instagram.com/eivindhalsnes/'),
     ('2', 'https://www.linkedin.com/in/eivind-hauge-halsnes-875542215/', 'https://github.com/ehalsnes', 'https://www.instagram.com/eivindhalsnes/'),
-    ('3', 'https://www.linkedin.com/in/eivind-hauge-halsnes-875542215/', 'https://github.com/ehalsnes', 'https://www.instagram.com/eivindhalsnes/');
-    ('4', 'https://www.linkedin.com/in/eivind-hauge-halsnes-875542215/', 'https://github.com/ehalsnes', 'https://www.instagram.com/eivindhalsnes/');
-    ('5', 'https://www.linkedin.com/in/eivind-hauge-halsnes-875542215/', 'https://github.com/ehalsnes', 'https://www.instagram.com/eivindhalsnes/');
+    ('3', 'https://www.linkedin.com/in/eivind-hauge-halsnes-875542215/', 'https://github.com/ehalsnes', 'https://www.instagram.com/eivindhalsnes/'),
+    ('4', 'https://www.linkedin.com/in/eivind-hauge-halsnes-875542215/', 'https://github.com/ehalsnes', 'https://www.instagram.com/eivindhalsnes/'),
+    ('5', 'https://www.linkedin.com/in/eivind-hauge-halsnes-875542215/', 'https://github.com/ehalsnes', 'https://www.instagram.com/eivindhalsnes/'),
     ('6', 'https://www.linkedin.com/in/eivind-hauge-halsnes-875542215/', 'https://github.com/ehalsnes', 'https://www.instagram.com/eivindhalsnes/');
 
 #Testdata company
@@ -27,10 +27,12 @@ INSERT INTO company(company_name, company_email, company_desc, company_url, comp
     ('Egde Consulting Kristiansand', 'egde@mail.com', 'Vi er et konsulent firma med et bredt spekter av kompetanse', 'www.egde.no', 'Gravane 16', 'Kristiansand', '4610', 'hemmelig'),
     ('Egde Consulting Grimstad', 'egde@mail.com', 'Vi er et konsulent firma med et bredt spekter av kompetanse', 'www.egde.no', 'Terje  Løvås vei 1', 'Grimstad', '4879', 'hemmelig'),
     ('Egde Consulting Porsgrunn', 'egde@mail.com', 'Vi er et konsulent firma med et bredt spekter av kompetanse', 'www.egde.no', 'Dokkvegen 11', 'Porsgrunn', '3920', 'hemmelig'),
-    ('Gard AS', 'gard@mail.com', 'Vi er et konsulent firma med et bredt spekter av kompetanse', 'www.gard.no', 'Bukkene Bruses vei 12', 'Kristiansand S', '4638', 'hemmelig');
-    ('Klippehopping AS', 'klippe@mail.com', 'Vi er et konsulentselskap som leverer et bredt aspekt av klippehoppere.', 'Kliphop.no', 'Ramnfloget 1', 'Bodø', '8011', 'hemmelig');
-    ('Spiseriet til mor AS', 'spisemor@mail.com', 'Vi er et selskap som driver med catering av konsulent-kunnskap', 'Spisemor.no', 'Bolleveien 45', 'Stavanger', '4900', 'hemmelig');
-    ('Pappas pizzeria', 'Papapiza@mail.com', 'Vi er et selskap med et bredet spekter av pizza-utvalg', 'Papapiza.no', 'Hjartliveien 3', 'Bodø', '8011', 'hemmelig');
+    ('Gard AS', 'gard@mail.com', 'Vi er et konsulent firma med et bredt spekter av kompetanse', 'www.gard.no', 'Bukkene Bruses vei 12', 'Kristiansand S', '4638', 'hemmelig'),
+    ('Klippehopping AS', 'klippe@mail.com', 'Vi er et konsulentselskap som leverer et bredt aspekt av klippehoppere.', 'Kliphop.no', 'Ramnfloget 1', 'Bodø', '8011', 'hemmelig'),
+    ('Spiseriet til mor AS', 'spisemor@mail.com', 'Vi er et selskap som driver med catering av konsulent-kunnskap', 'Spisemor.no', 'Bolleveien 45', 'Stavanger', '4900', 'hemmelig'),
+    ('Pappas pizzeria', 'Papapiza@mail.com', 'Vi er et selskap med et bredet spekter av pizza-utvalg', 'Papapiza.no', 'Hjartliveien 3', 'Bodø', '8011', 'hemmelig'),
+    ('Hallgeirs bakeri', 'Hallbakeri@mail.com', 'Jeg styrer en bedrift som jobbed med agil baking', 'Hallgeirsbakeri.no', 'Barstølveien 32', 'Kristiansand', '4630', 'hemmelig'),
+    ('Janis Jarnitsjarer', 'Janismusikk@mail.com', 'Dette er et korps som spesialiserer i hardware', 'Janismusikk.no', 'Utedoveien 3', 'Voss', '3210', 'hemmelig');
 
 #Testdata company_social
 INSERT INTO company_social(company_id, linkedin, github, instagram) VALUES 
