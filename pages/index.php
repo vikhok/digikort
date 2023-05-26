de5042fb6c44e4d5a7669c3e8de571b6a5887e83
AleksKol <kols203@gmail.com>
2023-05-26 13:11:59 +0200

    Endret i index.php ferdig

--- pages/index.php
+++ pages/index.php
@@ -60,8 +60,8 @@
     <div class="business-card-container">
         <div class="personal-information">
             <h2><?=$name?></h2>
-            <h2><a href="mailto:"<?=$email?></h2>
-            <h2><a href="tel:"<?=$phone?></h2>
+            <h2 class="business-card-mail"><a href="mailto:<?=$email?>"><?=$email?></a></h2>
+            <h2 class="business-card-number"><a href="tel:<?=$phone?>"><?=$phone?></a></h2>
             <br>
             <h2 class="business-card-company"><?=$company?></h2>
             <h2 class="business-card-job-title"><?=$job_title?></h2>
